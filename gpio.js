611bf673e16b7db015c782ae7afa60df67689faf
litc0de <litc0de@posteo.org>
2023-07-06 16:10:48 +0200

    add distance sensor

--- gpio.js
+++ gpio.js
@@ -1,17 +1,38 @@
 const Gpio = require('onoff').Gpio;
 
-const led = new Gpio(17, 'out');
-const button = new Gpio(4, 'in', 'both');
+const trigger = new Gpio(18, 'out');
+const echo = new Gpio(24, 'in');
 
-button.watch((err, value) => {
-  if (err) {
-    throw err;
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const getDistance = async () => {
+  trigger.writeSync(1);
+  await sleep(0.01);
+  trigger.writeSync(0);
+
+  let startTime;
+  let stopTime;
+
+  while (echo.readSync === 0) {
+    startTime = Date.now();
   }
 
-  led.writeSync(value);
-});
+  while (echo.readSync === 1) {
+    stopTime = Date.now();
+  }
+
+  const timeElapsed = stopTime - startTime;
+  const distance = (timeElapsed * 34300) / 2;
+
+  return distance;
+}
+
+const interval = setInterval(getDistance, 1000);
 
 process.on('SIGINT', (_) => {
-  led.unexport();
-  button.unexport();
+  triger.unexport();
+  echo.unexport();
+  clearInterval(interval);
 });
