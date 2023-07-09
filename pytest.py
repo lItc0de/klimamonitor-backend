ad4a1a7d171a2d694664d74e9f92df15dc0572f7
litc0de <litc0de@posteo.org>
2023-07-09 17:04:16 +0200

    8e9a1fc3-30d3-4272-91e7-428c0097dfa7

---
+++ pytest.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+
+try:
+	GPIO.setmode(GPIO.BCM)
+
+	PIN_LED = 17
+
+	GPIO.setup(PIN_LED, GPIO.OUT)
+
+	GPIO.output(PIN_LED, GPIO.LOW)
+
+	print("Waiting for sensor to settle")
+
+	time.sleep(2)
+
+	print("Calculating distance")
+
+	GPIO.output(PIN_LED, GPIO.HIGH)
+
+	time.sleep(2)
+
+	GPIO.output(PIN_LED, GPIO.LOW)
+
+	print("finish")
+
+finally:
+	GPIO.cleanup()
